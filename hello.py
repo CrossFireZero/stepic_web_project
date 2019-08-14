9bbdf2d7ee326c7cbe22cb3b386bb655bad9cf6a
Max <robotland.ru@gmail.com>
2019-08-14 21:28:22 +0300

    Cleaned hello.py

--- hello.py
+++ hello.py
@@ -5,9 +5,7 @@ def hello(env, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     body = '\n'.join(env['QUERY_STRING'].split('&'))
-    print(body)
     body = bytes(body, encoding='utf-8')
-    print(body)
     start_response(status, headers)
 
     return [body]
