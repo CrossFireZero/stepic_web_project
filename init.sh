9494230c9b03052c3c90c750eca333205763085c
Max <robotland.ru@gmail.com>
2019-08-14 22:13:52 +0300

    Changes to init #4.

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo rm  /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-available/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo service nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart﻿﻿
