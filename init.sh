dd397973155c5e206f5767801e0f000532198a56
Max <robotland.ru@gmail.com>
2019-08-29 22:02:42 +0300

    Creating db into init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo service nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart﻿﻿
+sudo /etc/init.d/mysql start
 mysql -uroot -e "create database ask;"
 mysql -uroot -e "grant all privileges on ask.* to 'box'@'localhost' with grant option;"
