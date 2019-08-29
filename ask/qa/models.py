d1676e365cd4cba582ce7a1c0d97466ca8135140
Max <robotland.ru@gmail.com>
2019-08-29 21:35:41 +0300

    Added models for Question, Answer, added QuestionManager, added initialization db.

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,41 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return super(QuestionManager, self).get_queryset().order_by('-added_at')
+
+    def popular(self):
+        return super(QuestionManager, self).get_queryset().order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='likes_set')
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return reverse('question', args=(self.id,))
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.text
+
+    def get_url(self):
+        return reverse('question', args=(self.question_id,))
--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo service nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart﻿﻿
+mysql -u root -e "CREATE DATABASE ask"
